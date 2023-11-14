954fd322a6fc2c331dbfdea4b5dffcbb4b9b61c2
John Galt <johngaltfirstrun@gmail.com>
2023-11-14 12:34:06 -0500

    signing: support inline signing for U
    For those who have used .android-certs previously, cp .android-certs/* vendor/custom/signing/keys/
    
    Change-Id: If8553fc9f89cbc0923abdae46e42218df14535bf
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- config/signing.mk
+++ config/signing.mk
@@ -1,9 +1,9 @@
 ifeq (user,$(TARGET_BUILD_VARIANT))
-ifneq (,$(wildcard .android-certs/releasekey.pk8))
-PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+ifneq (,$(wildcard vendor/custom/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/custom/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
-ifneq (,$(wildcard .android-certs/otakey.x509.pem))
-PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+ifneq (,$(wildcard vendor/custom/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/custom/signing/keys/otakey.x509.pem
 endif
 endif
