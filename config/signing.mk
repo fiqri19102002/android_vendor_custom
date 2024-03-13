1d39be4c76b3035fdad2588d375f833552d7b6ae
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    signing: Sign every build variant but eng
    Change-Id: Iad382aa2b2242b89014f1d659fdb4e764d0cca67
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- config/signing.mk
+++ config/signing.mk
@@ -1,4 +1,4 @@
-ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/custom/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/custom/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
