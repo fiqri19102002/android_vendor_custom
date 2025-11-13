2f7223b4bbec5ff60aa1288ea010fe72a9e5cb6a
Fiqri Ardyansyah <fiqri191002@gmail.com>
2025-11-13 08:18:27 +0000

    signing: Commented out enable oem unlock prop
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- config/signing.mk
+++ config/signing.mk
@@ -2,7 +2,7 @@ ifneq (,$(wildcard vendor/custom/signing/keys))
 ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/custom/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/custom/signing/keys/releasekey
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+#PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
 endif
 ifneq (,$(wildcard vendor/custom/signing/keys/otakey.x509.pem))
 PRODUCT_OTA_PUBLIC_KEYS := vendor/custom/signing/keys/otakey.x509.pem
